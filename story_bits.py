4aabc797cd535cab380c844217ecb3bcab1414f1
Dustin Clark <dclark2434@gmail.com>
2023-10-02 08:50:51 -0500

    refactored with seperation of story and logic

---
+++ story_bits.py
@@ -0,0 +1,73 @@
+INTRO = [
+
+"Beyond the mists lies the Labyrinth of Desolation, a tortured construct of sorrow and stone.",
+"Your purpose, a melancholic melody sung by the stars, is to seek and eradicate the twisted aberration: the Minotaurus.",
+"Venture forth with trepidation, for this serpentine maze drips with the tears of the fallen and craves for more."
+
+]
+
+CHOICE = [
+
+    """The ancient, scarred gateway of the labyrinth beckons.\n
+    Every brick seems to whisper tales of dread.\n
+    Softly, into the abyss, murmur 'ENTER' to descend into darkness.\n""",
+
+    """Deep within the labyrinth's corroded veins, echoes of primordial massacres linger.\n
+    To your right, the ground weeps with age-old blood, echoing with the broken prayers of long-forgotten champions.\n
+    Do you heed the mournful cries, murmuring 'LEFT'?\n
+    Do you pivot 'RIGHT', chasing phantoms?\n
+    Or do you stride 'STRAIGHT' into the chasm's embrace, ready to confront what malevolent forces await?\n""",
+
+    """An ancient door, its wood warped by screams of yesteryears, stands defiant.\n
+    Can you muster the courage? Whisper 'OPEN' to face the shadows within.\n""",
+
+    """The grotesque Minotaurus, an embodiment of torment, eyes you with bloodlust.\n
+    Will you challenge fate and darkness alike? Let 'FIGHT' slip past your trembling lips to wage war.\n""",
+
+    """Dread hangs thick. Yet, salvation or doom hinges on your armament's choice.\n
+    Whisper the dark anthem of your weapon: 'SWORD', 'AXE', 'SPEAR', 'BOW', 'FIST', or 'CHAINSAW'.\n""",
+
+]
+
+ENDING = [
+    
+    """Even the maze's threshold proves treacherous. The Minotaurus ensures your tale is brief and bleak.\n""",
+
+    """In your descent into the void, the vast expanse of emptiness envelopes you.\n
+    The Minotaurus might elude sight, yet the labyrinth itself ensnares your soul,\n
+    binding you in an unyielding, eternal captivity.\n""",
+
+    """The chilling resonance of hooves marks the prelude to darkness.\n
+    The Minotaurus relishes in claiming yet another wandering soul.\n""",
+
+    """Though the door remains unyielding, doom is ever-lurking. The Minotaurus, with a roar, marks your tragic end.\n""",
+
+    """Courage abandoned, you become yet another forgotten tale. The Minotaurus ensures your silence is eternal.\n""",
+
+    """Grasping the spear, destiny's chosen tool, you brace as the Minotaurus lunges.\n
+    It meets its cold end upon your instrument of fate.\n
+    Amidst the labyrinthine despair, you are the solitary beacon of triumph.\n
+    To depart this forsaken place, gently murmur 'DEPART' to the abyss.\n""",
+
+    """With the gleaming blade, you hope to rewrite fate.\n
+    Yet the Minotaurus, in its cruel agility, ensures your aspirations crumble.\n
+    The cold steel of its horns introduce you to eternal night.\n""",
+
+    """The weighty axe promises power, but the Minotaurus' dance is one of deadly grace.\n
+    Its horns find their home in your heart, ushering you into the void.\n""",
+
+    """With the somber bow of midnight's embrace, you notch an arrow woven from the very fabric of forgotten lamentations.\n
+    Yet as the shadowy missile seeks its mark, the Minotaurus, blessed by the dark void between two eternities, dances aside,\n
+    ensuring your descent into an abyss of despair.\n""",
+
+    """Your fists, bearing the weight of sins and salvations, pulse with a dark aura that seems birthed from the spaces between stars.\n
+    Each blow is a symphony of both creation and cataclysm. Before such primordial might, the Minotaurus crumples, \n
+    and the very walls of the labyrinth weep in reverence of your ethereal prowess.\n""",
+
+    """Awakening the chainsaw, its roar is a dirge echoing between heaven and hell.\n
+    It feasts, not just on flesh, but on the very spirit of the Minotaurus.\n
+    When the requiem of battle dwindles, you emerge, not just a victor, but as a bridge between two fires, unyielding and eternal.\n""",
+
+    """In the face of horror, time waits not for indecision. Your end is swift, rendered by the merciless Minotaurus.\n"""
+
+]
